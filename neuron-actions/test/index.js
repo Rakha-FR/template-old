d3ef210b981231ece092e7584d5fc3afdc1798da
Saman Supriadi <samansenju@gmail.com>
2025-09-17 14:47:14 +0700

    up

---
+++ neuron-actions/test/index.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const branch = github.context.ref.replace('refs/heads/', '');
+  console.log(`Current branch is: ${branch}`);
+  core.setOutput('branch', branch);
+} catch (error) {
+  core.setFailed(error.message);
+}
