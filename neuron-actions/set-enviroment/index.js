089f4b52231dd03c5246817cb0ec1f3bd90c7f91
Saman Supriadi <samansenju@gmail.com>
2025-09-17 15:47:29 +0700

    update

--- neuron-actions/set-enviroment/index.js
+++ neuron-actions/set-enviroment/index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+
 try {
   const branch = github.context.ref.replace('refs/heads/', '');
   console.log(`Current branch is: ${branch}`);
