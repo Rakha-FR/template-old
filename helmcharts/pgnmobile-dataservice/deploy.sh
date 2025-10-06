9d92c8679a4743eb1c69a37b322316bbd158bde7
Saman Supriadi <samansenju@gmail.com>
2025-10-06 15:56:13 +0700

    updatea

--- helmcharts/pgnmobile-dataservice/deploy.sh
+++ helmcharts/pgnmobile-dataservice/deploy.sh
@@ -10,13 +10,10 @@ if [ -z $APP_VERSION ]; then echo "APP_VERSION is required"; exit 1; fi
 if [ -z $VALUES_CHART ]; then echo "VALUES_CHART is required"; exit 1; fi
 if [ -z $NAMESPACE_KUBERNETES ]; then echo "NAMESPACE_KUBERNETES is required"; exit 1; fi
 
-
-echo " $APP_VERSION $VALUES_CHART $NAMESPACE_KUBERNETES "
-
-# kubectl config set-context --current --namespace="${NAMESPACE_KUBERNETES}"
-#     sed -i "s/^appVersion:.*$/appVersion: $APP_VERSION/" "./chart/data/Chart.yaml"
-#     helm upgrade data-services ./chart/data/ --values $VALUES_CHART --namespace=$NAMESPACE_KUBERNETES \
-#     --install \
-#     --set image.version=$APP_VERSION \
-#     --set name.space=$NAMESPACE_KUBERNETES \
-#     --wait
+kubectl config set-context --current --namespace="${NAMESPACE_KUBERNETES}"
+    sed -i "s/^appVersion:.*$/appVersion: $APP_VERSION/" "./chart/data/Chart.yaml"
+    helm upgrade data-services ./chart/data/ --values $VALUES_CHART --namespace=$NAMESPACE_KUBERNETES \
+    --install \
+    --set image.version=$APP_VERSION \
+    --set name.space=$NAMESPACE_KUBERNETES \
+    --wait
