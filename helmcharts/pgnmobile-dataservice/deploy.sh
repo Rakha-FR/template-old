5ab2e27839bd8002d15fa5de314f21cb0a556534
Saman Supriadi <samansenju@gmail.com>
2025-10-06 11:22:34 +0700

    update

---
+++ helmcharts/pgnmobile-dataservice/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+APP_VERSION="$1"
+VALUES_CHART="$2"
+NAMESPACE_KUBERNETES="$3"
+PROJECT_NAME="$4"
+URL="$5"
+
+if [ -z $APP_VERSION ]; then echo "APP_VERSION is required"; exit 1; fi
+if [ -z $VALUES_CHART ]; then echo "VALUES_CHART is required"; exit 1; fi
+if [ -z $NAMESPACE_KUBERNETES ]; then echo "NAMESPACE_KUBERNETES is required"; exit 1; fi
+
+
+kubectl config set-context --current --namespace="${NAMESPACE_KUBERNETES}"
+
+
+if [ "$PROJECT_NAME" == "mobile" ]; then
+   sed -i "s/^appVersion:.*$/appVersion: $APP_VERSION/" "./chart/data/Chart.yaml"
+   helm upgrade data-services ./chart/data/ --values $VALUES_CHART --namespace=$NAMESPACE_KUBERNETES \
+    --install \
+    --set image.version=$APP_VERSION \
+    --set name.space=$NAMESPACE_KUBERNETES \
+    --wait
+else
+    sed -i "s/^appVersion:.*$/appVersion: $APP_VERSION/" "./relyon/data/Chart.yaml"
+    helm upgrade data-services ./relyon/data/ --values $VALUES_CHART --namespace=$NAMESPACE_KUBERNETES \
+    --install \
+    --set image.version=$APP_VERSION \
+    --set name.space=$NAMESPACE_KUBERNETES \
+    --set "ingress.hosts[0].host=$URL" \
+        --set "ingress.tls[0].hosts[0]=$URL" \
+        --set serviceAccount.name=relyon-vault \
+        --set vault.annotations."vault\.hashicorp\.com/role"="relyon-development" \
+    --wait
+fi
