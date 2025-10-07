d39b167b7830756395906b5c297869563397a10e
Saman Supriadi <samansenju@gmail.com>
2025-10-07 13:23:28 +0700

    fix security hotspot

--- helmcharts/pgnmobile-dataservice/deploy.sh
+++ helmcharts/pgnmobile-dataservice/deploy.sh
@@ -10,22 +10,7 @@ if [ -z $APP_VERSION ]; then echo "APP_VERSION is required"; exit 1; fi
 if [ -z $VALUES_CHART ]; then echo "VALUES_CHART is required"; exit 1; fi
 if [ -z $NAMESPACE_KUBERNETES ]; then echo "NAMESPACE_KUBERNETES is required"; exit 1; fi
 
-if [[ "$PROJECT_NAME" == "mobile" ]]; then
-  echo "==== $PROJECT_NAME  mobix  ===="
-  kubectl config set-context --current --namespace="${NAMESPACE_KUBERNETES}"
 
-  sed -i "s/^appVersion:.*$/appVersion: $APP_VERSION/" "./mobile/data/Chart.yaml"
-
-  helm upgrade data-services ./mobile/data/ \
-    --values "$VALUES_CHART" \
-    --namespace "$NAMESPACE_KUBERNETES" \
-    --install \
-    --set image.version="$APP_VERSION" \
-    --set name.space="$NAMESPACE_KUBERNETES" \
-    --wait
-
-else
-  echo "==== $PROJECT_NAME   relyonnnn ===="
   sed -i "s/^appVersion:.*$/appVersion: $APP_VERSION/" "./relyon/data/Chart.yaml"
 
   helm upgrade data-services ./relyon/data/ \
@@ -35,4 +20,4 @@ else
     --set image.version="$APP_VERSION" \
     --set name.space="$NAMESPACE_KUBERNETES" \
     --wait
-fi
+
