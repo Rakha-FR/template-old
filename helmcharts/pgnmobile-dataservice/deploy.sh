9e0abb84351e1b10d3ad643468414380e0de0e2b
Saman Supriadi <samansenju@gmail.com>
2025-10-08 08:29:47 +0700

    update

--- helmcharts/pgnmobile-dataservice/deploy.sh
+++ helmcharts/pgnmobile-dataservice/deploy.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
 APP_VERSION="$1"
-VALUES_CHART="$2"
+ENV_SERVER="$2"
 NAMESPACE_KUBERNETES="$3"
 PROJECT_NAME="$4"
-
 
 if [ -z $APP_VERSION ]; then echo "APP_VERSION is required"; exit 1; fi
 if [ -z $VALUES_CHART ]; then echo "VALUES_CHART is required"; exit 1; fi
 if [ -z $NAMESPACE_KUBERNETES ]; then echo "NAMESPACE_KUBERNETES is required"; exit 1; fi
+if [ -z $PROJECT_NAME ]; then echo "PROJECT_NAME is required"; exit 1; fi
 
-    echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx $APP_VERSION $VALUES_CHART"
+kubectl config set-context --current --namespace="${NAMESPACE_KUBERNETES}"
 
-  sed -i "s/^appVersion:.*$/appVersion: $APP_VERSION/" "./relyon/data/Chart.yaml"
-  
+if [[ "$PROJECT_NAME" == *mobile* ]]; then
+  echo $PROJECT_NAME
+  sed -i "s/^appVersion:.*$/appVersion: $APP_VERSION/" "./mobile/data/Chart.yaml"  
+  helm upgrade data-services ./mobile/data/ \
+    --values "$VALUES_CHART" \
+    --namespace "$NAMESPACE_KUBERNETES" \
+    --install \
+    --set image.version="$APP_VERSION" \
+    --set name.space="$NAMESPACE_KUBERNETES" \
+    --wait
+else
+  echo $PROJECT_NAME
+  sed -i "s/^appVersion:.*$/appVersion: $APP_VERSION/" "./relyon/data/Chart.yaml"  
   helm upgrade data-services ./relyon/data/ \
     --values "$VALUES_CHART" \
     --namespace "$NAMESPACE_KUBERNETES" \
@@ -21,4 +32,4 @@ if [ -z $NAMESPACE_KUBERNETES ]; then echo "NAMESPACE_KUBERNETES is required"; e
     --set image.version="$APP_VERSION" \
     --set name.space="$NAMESPACE_KUBERNETES" \
     --wait
-
+fi
