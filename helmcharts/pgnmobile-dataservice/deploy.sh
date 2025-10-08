b2cbbdcc56854498dd69211757da79902e7b92e5
Saman Supriadi <samansenju@gmail.com>
2025-10-08 09:13:53 +0700

    update

--- helmcharts/pgnmobile-dataservice/deploy.sh
+++ helmcharts/pgnmobile-dataservice/deploy.sh
@@ -13,7 +13,7 @@ if [ -z $PROJECT_NAME ]; then echo "PROJECT_NAME is required"; exit 1; fi
 kubectl config set-context --current --namespace="${NAMESPACE_KUBERNETES}"
 
 if [[ "$PROJECT_NAME" == *mobile* ]]; then
-  echo $PROJECT_NAME
+  echo "$PROJECT_NAME mobile"
   sed -i "s/^appVersion:.*$/appVersion: $APP_VERSION/" "./mobile/data/Chart.yaml"  
   helm upgrade data-services ./mobile/data/ \
     --values "$VALUES_CHART" \
@@ -23,7 +23,7 @@ if [[ "$PROJECT_NAME" == *mobile* ]]; then
     --set name.space="$NAMESPACE_KUBERNETES" \
     --wait
 else
-  echo $PROJECT_NAME
+  echo "$PROJECT_NAME relyon"
   sed -i "s/^appVersion:.*$/appVersion: $APP_VERSION/" "./relyon/data/Chart.yaml"  
   helm upgrade data-services ./relyon/data/ \
     --values "$VALUES_CHART" \
