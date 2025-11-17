e0f088a35f5b1b06a6f6e7f38e369415255e3e00
Saman Supriadi <samansenju@gmail.com>
2025-11-17 11:40:56 +0700

    update

---
+++ collection/pgnmobile-pgnfullfilmentservice/helm/deploy.sh
@@ -0,0 +1,25 @@
+APP_VERSION="$1"
+ENV_SERVER="$2"
+NAMESPACE_KUBERNETES="$3"
+PROJECT_NAME="$4"
+
+if [ -z $APP_VERSION ]; then echo "APP_VERSION is required"; exit 1; fi
+if [ -z $ENV_SERVER ]; then echo "ENV_SERVER is required"; exit 1; fi
+if [ -z $NAMESPACE_KUBERNETES ]; then echo "NAMESPACE_KUBERNETES is required"; exit 1; fi
+if [ -z $PROJECT_NAME ]; then echo "PROJECT_NAME is required"; exit 1; fi
+
+kubectl config set-context --current --namespace="${NAMESPACE_KUBERNETES}"
+if [[ "$PROJECT_NAME" != *"development-pdp"* && "$PROJECT_NAME" != *"relyon"* ]]; then
+      echo "mobie ===="  
+elif [[ "$PROJECT_NAME" == *"relyon"* ]]; then
+    echo "relyon ===="
+    helm upgrade pgnmobile-pgnfullfilmentservice ./relyon/ \
+        --values ./relyon/values-$ENV_SERVER.yaml \
+        --namespace "$NAMESPACE_KUBERNETES" \
+        --install \
+        --set image.version="$APP_VERSION" \
+        --set name.space="$NAMESPACE_KUBERNETES" \
+        --wait
+elif [[ "$PROJECT_NAME" == *"development-pdp"* ]]; then
+      echo "pdp ===="  
+fi
